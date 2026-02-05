88653d2e9e3a3a4de47a949d0f3b71c50d44c75c
unknown <hmy1304@kbu.ac.kr>
2026-02-05 12:37:02 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
 import React, {useState} from 'react'
-import Counters from './components/counters'
-import Exam1 from './components/Exam1'
-import Exam2 from './components/Exam2'
-import Exam3 from './components/Exam3'
-import Inputfocus from './components/Inputfocus'
-import Counter2 from './components/Counter2'
-import Lifecycle from './components/Lifecycle'
+import Parent from './components/Parent'
 
 const App = () => {
-  const [state, setState] = useState(true)
-  const user={
-    name:'Alice',
-    age:21
-  }
+  
 
   return (
     <div>
-      <button onClick={()=>setState(true)}>마운트</button>
-      <button onClick={()=>setState(false)}>언마운트</button>
-      {state && 
-        <Lifecycle />
-      }
-      <hr />
-      <Counter2 />
-      <Inputfocus />
-      <Exam3 />
-      <Exam2 />
-      <Exam1 {...user}/>
-      <Counters />
+      <Parent />
     </div>
   )
 }
